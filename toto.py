6942a09db97c104db60858124daa182bbddfc3ec
djmMax <domguiajm@yahoo.fr>
2020-07-02 02:18:44 +0200

    delete code

--- toto.py
+++ toto.py
@@ -7,11 +7,4 @@ print('Toto is the best in Africa?')
 print('joseph')
 print('another change')
 
-import matplotlib.pyplot as pyplot
-x = np.random.randint(8,8, 20)
-print(x)
-y = np.sin(x)
-print(y)
-
-print('Test')
 
