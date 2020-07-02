45bdc295e25add44dfda8bf2b19c9f6c9f4d36e7
amidumohammed <mohammedamidu@ymail.com>
2020-07-02 02:10:55 +0200

    first change

--- toto.py
+++ toto.py
@@ -7,3 +7,9 @@ print('Toto is the best in Africa?')
 print('joseph')
 print('another change')
 
+import matplotlib.pyplot as pyplot
+x = np.random.randint(8,8, 20)
+print(x)
+y = np.sin(x)
+print(y)
+
